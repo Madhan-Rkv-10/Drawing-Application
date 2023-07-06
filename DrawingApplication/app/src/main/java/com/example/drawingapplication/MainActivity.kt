0ee8b1631db8edb0066c3d9469b67f6dff4c3377
Madhan-Rkv <madhanrkv10@gmail.com>
2023-07-06 12:06:11 +0530

    adding Storage Permission

--- DrawingApplication/app/src/main/java/com/example/drawingapplication/MainActivity.kt
+++ DrawingApplication/app/src/main/java/com/example/drawingapplication/MainActivity.kt
@@ -1,18 +1,48 @@
 package com.example.drawingapplication
 
+import android.app.AlertDialog
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import android.Manifest
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     private var myActiveColorImageButton: ImageButton? = null
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permission ->
+            permission.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+                if (isGranted) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Permission granted now you can read the storage files.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    //perform operation
+                } else {
+                    //Todo 4: Displaying another toast if permission is not granted and this time focus on
+                    //    Read external storage
+                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE)
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Oops you just denied the permission.",
+                            Toast.LENGTH_LONG
+                        ).show()
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,6 +56,10 @@ class MainActivity : AppCompatActivity() {
         val ib_brush: ImageButton = findViewById(R.id.ib_brush)
         ib_brush.setOnClickListener {
             showSizeButtonDialog()
+        }
+        val ib_gallery = findViewById<ImageButton>(R.id.ic_gallery)
+        ib_gallery.setOnClickListener {
+            RequestStoragePermission()
         }
 
     }
@@ -48,8 +82,7 @@ class MainActivity : AppCompatActivity() {
             )
             myActiveColorImageButton?.setImageDrawable(
                 ContextCompat.getDrawable(
-                    this,
-                    R.drawable.pallet_normal
+                    this, R.drawable.pallet_normal
                 )
             )
 
@@ -85,5 +118,38 @@ class MainActivity : AppCompatActivity() {
             brushDialog.dismiss()
         }
         brushDialog.show()
+    }
+
+    private fun RequestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            showRationaleDialog(
+                "Drawing APP",
+                "Drawing App Need to Access your External Storage"
+            )
+        } else {
+            requestPermission.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            )
+        }
+    }
+
+    private fun showRationaleDialog(
+        title: String,
+        message: String,
+    ) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
     }
 }
